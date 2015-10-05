6017c82c4098d26a7a5bd668ff2ccba7d5077e66
Dmitry Kravtsov <dmittelmaier@gmail.com>
2015-10-05 12:08:56 +0300

    TweetsChart v1.0.0

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,8 @@
 module TweetsHelper
+	def tweets_chart_series
+    (@start_time..DateTime.now.to_i).step(@step).each_cons(2).map do |period| 
+      Tweet.count_on(period[0], period[1]) 
+    end
+    .to_a
+  end
 end
